cfa8ab442c933e1a9291b0ff245a567f84caf860
alvarogomez7 <alvaroigomezj96@gmail.com>
2024-09-03 20:38:21 -0300

    Iniciando la rama del Frontend

---
+++ TuFulbito-FE/src/Components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+    const [user, setUser] = useState({
+        emai: '',
+        password: ''
+    })
+  
+    return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
