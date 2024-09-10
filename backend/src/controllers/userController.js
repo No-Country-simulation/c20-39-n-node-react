b7031e73b54cb8ad7f901c7e050a1f28899acf95
pablete18 <pabloealcaraz@gmail.com>
2024-09-09 22:46:59 -0300

    [feat] agrego api login & rutas!

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,6 +3,7 @@ const sequelize = db.sequelize
 const bcryptjs = require('bcrypt')
 const { validationResult} = require('express-validator')
 
+
 module.exports={
     registerUser: async(req,res)=>{
          let errors = validationResult(req)
@@ -37,5 +38,49 @@ module.exports={
         })
         
 
+    },
+    loginUser:async(req,res)=>{
+        let errors = validationResult(req)
+
+        if(errors.isEmpty()){
+            try {
+                const {email,password}= req.body
+    
+                let userInDb = await db.User.findOne(
+                    {
+                    where : {email},
+                    attributes :{
+                         exclude : ['createdAt','updatedAt','userId']
+                        }
+                    }
+                )
+                
+                let isOkPass = await bcryptjs.compare(password,userInDb.password)
+    
+                if(userInDb && isOkPass){
+                    return res.status(200).json({
+                        ok:true,
+                        msg : "Usuario logueado exitosamente",
+                        data : userInDb                    
+                    })
+                }else if (!isOkPass){
+                    return res.status(400).json({
+                        ok : false,
+                        error : "credenciales invalidas"
+                    })
+                }
+            } catch (error) {
+                return res.status(error.status || 500).json({
+                    ok:false,
+                    msg : error.message
+                })
+            }        
+        }
+            return res.status(400).json({
+                ok:false,
+                errors : errors.array()
+            })
+        
+       
     }
 }
